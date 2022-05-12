53af16223573ac28daaf58825eab81f24aa33b88
roymondchen <roymondchen@tencent.com>
2022-05-12 16:17:19 +0800

    feat: update

--- playground/runtime/react/assets/components.js
+++ playground/runtime/react/assets/components.js
@@ -1,1 +1,1 @@
-import{P as t,C as n,a as e,T as o}from"./Text.0931af7a.js";const a={page:t,container:n,button:e,text:o},s={},r={components:a,plugins:s};window.magicPresetComponents=r;export{r as e};
+import{P as t,C as n,a as e,T as o}from"./Text.2050a1d2.js";const a={page:t,container:n,button:e,text:o},s={},r={components:a,plugins:s};window.magicPresetComponents=r;export{r as e};
