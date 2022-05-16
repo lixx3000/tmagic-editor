c340107994db2a51f612a8c43866a04bf8bd5521
roymondchen <roymondchen@tencent.com>
2022-05-16 21:41:39 +0800

    feat: update docs

---
+++ docs/assets/color-picker.html.f3ce0d6e.js
@@ -0,0 +1,1 @@
+const e={key:"v-05aab9bd",path:"/api/form-config/fields/color-picker.html",title:"ColorPicker \u989C\u8272\u9009\u62E9\u5668",lang:"en-US",frontmatter:{},excerpt:"",headers:[{level:2,title:"\u57FA\u7840\u7528\u6CD5",slug:"\u57FA\u7840\u7528\u6CD5",children:[]}],filePathRelative:"api/form-config/fields/color-picker.md"};export{e as data};
