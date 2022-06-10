7ebac96f6e62a37d7ba28d3b8316857d405ad023
roymondchen <roymondchen@tencent.com>
2022-06-10 12:21:27 +0800

    feat: update playground

--- playground/runtime/react/assets/components.js
+++
@@ -1,1 +0,0 @@
-import{P as e,C as a,a as o,I as t,T as n,Q as s,O as r}from"./Text.ca392f4c.js";const c={page:e,container:a,button:o,img:t,text:n,qrcode:s,overlay:r},g={},i={components:c,plugins:g};window.magicPresetComponents=i;export{i as e};
--- playground/runtime/vue2/assets/config.js
+++
@@ -1,1 +0,0 @@
-var e=[],a=[],r=[],n=[],o=[{text:"\u56FE\u7247",name:"src"},{text:"\u94FE\u63A5",name:"url"}],u=[{text:"\u94FE\u63A5",name:"url"}],c=[];(function(){const t={page:e,container:a,button:r,text:n,img:o,qrcode:u,overlay:c};window.magicPresetConfigs=t})();
