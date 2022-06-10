b8ba9f8c7d4e19f15b0a7a5782fe84ec98da3b50
roymondchen <roymondchen@tencent.com>
2022-06-10 14:47:06 +0800

    fix: 添加runtime

---
+++ playground/runtime/react/assets/components.js
@@ -0,0 +1,2 @@
+import{P as e,C as a,a as o,I as t,T as n,Q as s,O as r}from"./Text.35d811ca.js";const c={page:e,container:a,button:o,img:t,text:n,qrcode:s,overlay:r},g={},i={components:c,plugins:g};window.magicPresetComponents=i;export{i as e};
+//# sourceMappingURL=components.js.map
